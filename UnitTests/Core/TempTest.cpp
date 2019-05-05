30595db4a6970eea540b9e9e9a82996792e71a6c
Michaël Larouche <michael.larouche@gmail.com>
2019-05-05 12:04:57 -0400

    Setup of build system is done for now

---
+++ UnitTests/Core/TempTest.cpp
@@ -0,0 +1,7 @@
+// Google test includes
+#include <gtest/gtest.h>
+
+TEST(TempTest, SampleTest)
+{
+	ASSERT_TRUE(true);
+}
--- genie.lua
+++ genie.lua
@@ -1,5 +1,78 @@
+function baroqueProject(name, projectKind)
+    project(name)
+        kind(projectKind)
+
+    configuration "*"
+        flags { "ExtraWarnings", "FatalWarnings", "NoExceptions", "NoRTTI", "Cpp17" }
+        includedirs { "." }
+
+    configuration "Debug"
+        flags { "Symbols" }
+
+    configuration "OptimizedDebug"
+        flags { "Symbols", "OptimizeSpeed" }
+
+    configuration "Profile"
+        flags { "OptimizeSpeed" }
+
+    configuration "Retail"
+        flags { "OptimizeSpeed" }
+
+    configuration "*"
+end
+
+function baroqueStaticLib(name)
+    baroqueProject(name, "StaticLib")
+end
+
+function baroqueStaticDependency(name)
+    project(name)
+        kind("StaticLib")
+
+    configuration "Debug"
+        flags { "Symbols" }
+
+    configuration "OptimizedDebug"
+        flags { "Symbols", "OptimizeSpeed" }
+
+    configuration "Profile"
+        flags { "OptimizeSpeed" }
+
+    configuration "Retail"
+        flags { "OptimizeSpeed" }
+
+    configuration "*"
+end
+
+function useDependency(dependencyInfo)
+    includedirs(dependencyInfo.includes)
+    links(dependencyInfo.links)
+end
+
+function baroqueUnitTest(name)
+    baroqueProject(name, "ConsoleApp")
+        links { "gtest" }
+        includedirs {
+            "3rdparty/gtest/include/"
+        }
+        configuration "vs*"
+            buildoptions { "/EHsc" }
+end
+
 solution "BaroqueEngine"
     location "Projects"
+
+    configuration "Debug"
+        targetdir "bin/Debug"
+
+    configuration "OptimizedDebug"
+        targetdir "bin/OptimizedDebug"
+
+    configuration "Profile"
+        targetdir "bin/Profile"
+
+    configuration "Retail"
+        targetdir "bin/Retail"
 
     configurations {
         "Debug",
@@ -14,43 +87,30 @@ solution "BaroqueEngine"
 
     language "C++"
 
-    project "gtest"
-        kind "StaticLib"
-
-        files {
-            "3rdparty/gtest/src/*.cc"
-        }
-
-        includedirs {
-            "3rdparty/gtest/include/"
-        }
-
-    project "Core"
-        kind "StaticLib"
-
+    baroqueStaticLib "Core"
         files {
             "Core/**.cpp",
             "Core/**.h"
         }
 
-        configuration "*"
-            flags { "ExtraWarnings", "FatalWarnings", "NoExceptions", "NoRTTI", "Cpp17" }
+    group "3rdparty"
 
-        configuration "Debug"
-            flags { "Symbols" }
-
-        configuration "OptimizedDebug"
-            flags { "Symbols", "OptimizeSpeed" }
-
-        configuration "Profile"
-            flags { "OptimizeSpeed" }
-
-        configuration "Retail"
-            flags { "OptimizeSpeed" }
-
-    project "CoreUnitTests"
+    baroqueStaticDependency "gtest"
         files {
-            "UnitTests/Core/**.cpp"
+            "3rdparty/gtest/src/gtest-all.cc"
+        }
+
+        includedirs {
+            "3rdparty/gtest/include/",
+            "3rdparty/gtest/"
+        }
+
+    group "UnitTests"
+
+    baroqueUnitTest "CoreUnitTest"
+        files {
+            "UnitTests/Core/**.cpp",
             "UnitTests/Core/**.h"
         }
-        links { "Core", "gtest" }
+
+        links { "Core" }
